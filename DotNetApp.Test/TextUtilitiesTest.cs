4c9f75050857a4edce51d5060fac79f099fb82e4
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-10-15 17:14:09 +0300

    Add text utilities.

---
+++ DotNetApp.Test/TextUtilitiesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DotNetApp.Collections.Extensions;
+
+namespace DotNetApp.Test
+{
+    public class TextUtilitiesTest
+    {
+        [Theory]
+        [InlineData("test", "test", 0)]
+        [InlineData("test2", "test", 1)]
+        [InlineData("kitten", "sitting", 3)]
+        [InlineData("saturday", "sunday", 3)]
+        public void CanCalculateLevenshteinDistance(string a, string b, int expected)
+        {
+            Assert.Equal(Utilities.Text.LevenshteinDistance(a, b), expected);
+        }
+    }
+}
---
+++ DotNetApp/Utilities/Text.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetApp.Utilities
+{
+    public static class Text
+    {
+        [Flags]
+        public enum MatchingOptions
+        {
+            Exact = 0,
+            CaseInsensitive = 1,
+            Trim = 2,
+            Default = CaseInsensitive | Trim
+        }
+
+        public static int LevenshteinDistance(string a, string b, MatchingOptions flags = MatchingOptions.Default)
+        {
+            a = a ?? string.Empty;
+            b = b ?? string.Empty;
+
+            if (flags.HasFlag(MatchingOptions.CaseInsensitive))
+            {
+                a = a.ToLower();
+                b = b.ToLower();
+            }
+
+            if (flags.HasFlag(MatchingOptions.Trim))
+            {
+                a = a.Trim();
+                b = b.Trim();
+            }
+
+            return LevenshteinDistance(a.AsSpan(), b.AsSpan());
+        }
+
+        private static int LevenshteinDistance(ReadOnlySpan<char> a, ReadOnlySpan<char> b)
+        {
+            if (a.Length == 0) return b.Length;
+            if (b.Length == 0) return a.Length;
+
+            int[,] matrix = new int[a.Length + 1, b.Length + 1];
+
+            for (int i = 0; i <= a.Length; ++i)
+            {
+                matrix[i, 0] = i;
+            }
+
+            for (int j = 1; j <= b.Length; ++j)
+            {
+                matrix[0, j] = j;
+            }
+
+            for (int i = 1; i <= a.Length; ++i)
+            {
+                for (int j = 1; j <= b.Length; ++j)
+                {
+                    int cost = a[i - 1] == b[j - 1] ? 0 : 1;
+                    cost += matrix[i - 1, j - 1];
+                    int cost_i = 1 + matrix[i - 1, j];
+                    int cost_j = 1 + matrix[i, j - 1];
+                    cost = cost < cost_i ? cost : cost_i;
+                    cost = cost < cost_j ? cost : cost_j;
+                    matrix[i,j] = cost;
+                }
+            }
+
+            return matrix[a.Length, b.Length];
+        }
+    }
+}
