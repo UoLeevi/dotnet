5f5d85e50083c362f746c29f1d7dc9f253d75ef2
Leevi Uotinen <uotinen.leevi@hotmail.com>
2021-10-23 11:03:04 +0300

    Add dynamic event subscription capability.

---
+++ DotNetApp.Test/DynamicTest.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace DotNetApp.Test
+{
+    public class DynamicTest
+    {
+        [Fact]
+        public void DoesGetPropertyValue()
+        {
+            var dummy = new Dummy();
+
+            var value = Dynamic.GetPropertyOrFieldValue<Dummy, string>(dummy, nameof(Dummy.Property));
+            Assert.True(value == dummy.Property);
+
+            dummy.Property = "asdf";
+            value = Dynamic.GetPropertyOrFieldValue<Dummy, string>(dummy, nameof(Dummy.Property));
+            Assert.True(value == dummy.Property);
+        }
+
+        [Fact]
+        public void DoesSubscribeToEvent()
+        {
+            var dummy = new Dummy();
+            int counter = 0;
+
+            var unsubscribe = Dynamic.SubscribeToEvent(dummy, nameof(Dummy.PropertyChanged), () => ++counter);
+            Assert.True(counter == 0);
+
+            dummy.Property = "asdf";
+            Assert.True(counter == 1);
+
+            dummy.Property = "123";
+            Assert.True(counter == 2);
+
+            unsubscribe();
+            Assert.True(counter == 2);
+
+            dummy.Property = "qwe";
+            Assert.True(counter == 2);
+        }
+    }
+}
--- DotNetApp/Dynamic.cs
+++ DotNetApp/Dynamic.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace DotNetApp
 {
     public static class Dynamic
     {
+        private static readonly MethodInfo actionMethodInfo;
+
+        static Dynamic()
+        {
+            actionMethodInfo = typeof(Action).GetMethod(nameof(Action.Invoke));
+        }
+
+        private static class Store
+        {
+            internal static readonly ConcurrentDictionary<(Type type, string eventName), (EventInfo eventInfo, ParameterExpression[] parameters)> Events = new ConcurrentDictionary<(Type, string), (EventInfo, ParameterExpression[])>();
+            internal static (EventInfo eventInfo, ParameterExpression[] parameters) CreateEventInfo((Type type, string eventName) key)
+            {
+                (Type type, string eventName) = key;
+                var eventInfo = type.GetEvent(eventName);
+                var methodInfo = eventInfo.EventHandlerType.GetMethod(nameof(Action.Invoke));
+                var parameters = methodInfo
+                    .GetParameters()
+                    .Select(p => Expression.Parameter(p.ParameterType))
+                    .ToArray();
+
+                return (eventInfo, parameters);
+            }
+        }
+
         private static class Store<TSource, TValue>
         {
             internal static readonly ConcurrentDictionary<(Type type, string propertyOrFieldName), Func<TSource, TValue>> Getters = new ConcurrentDictionary<(Type type, string propertyOrFieldName), Func<TSource, TValue>>();
@@ -25,6 +51,23 @@ namespace DotNetApp
         {
             var getValue = Store<TSource, TValue>.Getters.GetOrAdd((source.GetType(), propertyOrFieldName), Store<TSource, TValue>.CreateGetter);
             return getValue(source);
+        }
+
+        public static Action SubscribeToEvent<TSource>(TSource source, string eventName, Action action)
+            where TSource : class
+        {
+            var (eventInfo, parameters) = Store.Events.GetOrAdd((source.GetType(), eventName), Store.CreateEventInfo);
+            var call = Expression.Call(Expression.Constant(action), actionMethodInfo);
+            var lambda = Expression.Lambda(eventInfo.EventHandlerType, call, parameters);
+            var handler = lambda.Compile();
+            eventInfo.AddEventHandler(source, handler);
+            var wrSource = new WeakReference<TSource>(source);
+            var wrHandler = new WeakReference<Delegate>(handler);
+            return () =>
+            {
+                if (!wrSource.TryGetTarget(out var s) || !wrHandler.TryGetTarget(out var h)) return;
+                eventInfo.RemoveEventHandler(s, h);
+            };
         }
     }
 }
